0c9454d63b368b77e179292334f431cdc4975145
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-07 16:05:38 -0400

    replaced getID with getByPk

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -5,38 +5,24 @@ const { User } = require("../../db/models");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
-  
-  async function verifyUser () {
-    const user = await User.findOne({
-      where: {
-        username: req.user.username,
-      },
-    });
-    if(!user){
-      return res.status(401).json({ error: "Wrong username and/or password" });
-    }
-    if (req.user.password() != user.password()){
-      console.log({ error: "Not properly authenticated- message denied" })
-      return res.status(401).json({ error: "Not properly authenticated- message denied"});
-    }
-  }
 
   try {
     if (!req.user) {
       return res.sendStatus(401);
     }
 
-    //this checks if the user if properly verified. If they aren't, it throws an error, and prevents the message from being sent
-    const verifying = await verifyUser()
-
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
     // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId && conversationId === await Conversation.getID(senderId, recipientId)) {
+    if (conversationId) {
+      let specificConvo = await Conversation.findByPk(conversationId)
+
       //even if we have a conversationId- we still have to check if the two parties are actually a part of that conversation
+      if (specificConvo.user1Id === senderId && specificConvo.user2Id === recipientId){
           const message = await Message.create({ senderId, text, conversationId});
           return res.json({ message, sender });
+      }
     }
 
 
