cc96c5c43c9322bb6384890287d278e200129428
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-07 16:04:49 -0400

    removed the getID function

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -21,21 +21,4 @@ Conversation.findConversation = async function (user1Id, user2Id) {
   return conversation;
 };
 
-//gets the ID of the conversation given the two parties
-Conversation.getID = async function (user1Id, user2Id) {
-  const conversation = await Conversation.findOne({
-    where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
-  });
-
-  // returns the Id of the conversation between the two parties
-  return conversation.dataValues.id;
-};
-
 module.exports = Conversation;
