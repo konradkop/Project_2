02736dec2689b0ea71d21fbdd8c5f5af1c5353d1
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-07 16:03:09 -0400

    removed the sort and transferred it server-side

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,25 +1,20 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
-import moment from "moment";
+ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
-  function sortTime(a, b) {
-    return moment(a.props.sortingTime).diff(moment(b.props.sortingTime)) 
-  }
-
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} sortingTime={message.createdAt}  />
+          <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} sortingTime={message.createdAt}/>
+          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser}/>
         );
-      }).sort(sortTime)}
+      })}
     </Box>
   );
 };
