08ad170243ad5565fecd2cb7a82f89c2e8f9d0c9
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-08 17:40:14 -0400

    made DATABASE_URL into purely env

--- server/db/db.js
+++ server/db/db.js
@@ -2,18 +2,8 @@
 
 const Sequelize = require("sequelize");
 
-if(process.env.DATABASE_URL){
-    var db = new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      logging: false
-    });
-  }
-else{
-    var db = new Sequelize('messenger',  'hatchways', '12345', {
-      host: 'localhost',
-      dialect: 'postgres',
-      logging: false
-  })
-}
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger",{
+  logging: false
+});
 
 module.exports = db;
