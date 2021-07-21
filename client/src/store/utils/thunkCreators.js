95a151bf00c0165d1423f6020699147bc34f4c5f
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-21 18:18:18 -0400

    new api to read messages

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -91,6 +91,10 @@ const sendMessage = (data, body) => {
   });
 };
 
+export const readMessages = async (body) => {
+  await axios.put("/api/messages/read", body);
+};
+
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
 export const postMessage = (body) => async (dispatch) => {
