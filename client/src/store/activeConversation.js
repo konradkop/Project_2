29a553ac646a0fd66be14e207eec2883cfa80e9f
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-21 18:17:28 -0400

    small stylistic change

--- client/src/store/activeConversation.js
+++ client/src/store/activeConversation.js
@@ -1,20 +1,29 @@
 const SET_ACTIVE_CHAT = "SET_ACTIVE_CHAT";
 
-export const setActiveChat = (username) => {
+export const setActiveChat = (otherUser) => {
   return {
     type: SET_ACTIVE_CHAT,
-    username
+    payload: {otherUser}
   };
 };
+
+
+//to change the messages to be read
+export const changeActiveUser = (state, payload) => {
+  return payload.otherUser.otherUser.username
+};
+
+
 
 const reducer = (state = "", action) => {
   switch (action.type) {
     case SET_ACTIVE_CHAT: {
-      return action.username;
+      return changeActiveUser(state, action.payload)
     }
     default:
       return state;
   }
 };
+
 
 export default reducer;
