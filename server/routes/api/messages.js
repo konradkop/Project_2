4f3996dda39ea0de3c99ead0730117d96a79628a
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-21 01:18:05 -0400

    moved the readMessages endpont entirely over to messages

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -5,7 +5,6 @@ const { User } = require("../../db/models");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
-
   try {
     if (!req.user) {
       return res.sendStatus(401);
@@ -16,12 +15,29 @@ router.post("/", async (req, res, next) => {
     
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      let specificConvo = await Conversation.findByPk(conversationId)
 
+      let specificConvo = await Conversation.findByPk(conversationId)
       //even if we have a conversationId- we still have to check if the two parties are actually a part of that conversation
-      if (specificConvo.user1Id === senderId){
-          const message = await Message.create({ senderId, text, conversationId, isSeen: false});
-          return res.json({ message, sender });
+      if (specificConvo.dataValues.user1Id === senderId || specificConvo.dataValues.user2Id === senderId){
+
+      //getting the messages that haven't been Seen yet
+      const messageList = await Message.findAll({
+        where:{
+          conversationId: conversationId,
+          isSeen: false
+        } 
+      })
+      for(let x = 0; x < messageList.length; x++){ 
+        if(messageList[x].dataValues.senderId !== senderId){
+          messageList[x].dataValues.isSeen = true
+          messageList[x].changed("isSeen", true)
+            await messageList[x].save()
+            await messageList[x].reload()
+        }
+    }
+
+        const message = await Message.create({ senderId, text, conversationId, isSeen: false});
+        return res.json({ message, sender });
       }
     }
 
