4d6e0b126d0fa236a3e073b0afd1af3cf99c61f4
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-07-25 00:59:10 -0400

    moved styles to seperate file

--- client/src/Login.js
+++ client/src/Login.js
@@ -11,107 +11,8 @@ import {
   Icon,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
-import { makeStyles } from "@material-ui/core/styles";
-import backgroundImage from "./images/bg.png" 
 import bubble from "./images/bubble.svg" 
-const useStyles = makeStyles((theme) => ({
-
-  root: {
-    display: "flex",
-    height: "100vh",
-    [theme.breakpoints.down("600")]: {
-      display: "grid",
-    }
-  },
-  image:{ 
-    display: "flex",
-    flexGrow: 1,
-    //converted the colors to rgba since it supports opacity better
-    backgroundImage: "linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)), url(" + backgroundImage + ")",
-    backgroundRepeat: "no-repeat;",
-    backgroundSize: "cover",
-    alignItems: "center", 
-    justifyContent: "center",
-    maxWidth: "40%",
-    [theme.breakpoints.down("600")]: {
-      maxWidth: "100%"
-    }
-  },
-
-  imageText:{ 
-    margin: 5,
-    alignItems: "center", 
-    justifyContent: "center",
-    textAlign: "center"
-  },
-  imageWords:{ 
-    fontSize: 40,
-    color: "white",
-    margin: 40
-  },
-
-  mainScreen:{ 
-    flexGrow: 5,
-    height: "80%",
-    padding: 40
-  },
-
-  form:{
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center", 
-    height: "80%",
-    justifyContent: "center",
-  },
-
-  innerForm:{
-    margin: 10,
-  },
-
-  header:{
-    justifyContent:"flex-end",
-    alignItems: "center",
-    [theme.breakpoints.down("600")]: {
-      justifyContent:"center",
-      marginBottom: "20px"
-    }
-  },
-
-  secondaryButton:{
-    color: "#3A8DFF",
-    backgroundColor: "white",
-    padding: "12px 28px"
-  },
-
-  textLarge:{
-    fontSize: 45,
-    fontWeight: "bold",
-    [theme.breakpoints.down("600")]: {
-      fontSize: 30,
-    }
-  },
-
-  textGrey:{
-  color: "grey",
-  paddingRight: 20,
-  },
-
-  primaryButtonContainer:{
-    display: "flex",
-    marginTop: 50,
-    justifyContent:"center",
-  },
-
-  primaryButton:{
-    color: "white",
-    backgroundColor: "#3A8DFF",
-    padding: "12px 70px",
-    fontSize: 20,
-    fontWeight: "bold",
-  }
-
-}));
-
+import { useStyles } from './styles';   
 
 const Login = (props) => {
   const classes = useStyles();
@@ -137,7 +38,7 @@ const Login = (props) => {
         <Grid className={classes.image}>
           <Grid className={classes.imageText}>
             <Icon>
-              <img src={bubble} alt="bubble" height="100" width="100"/>
+              <img src={bubble} alt="bubble" class={classes.icon}/>
             </Icon>
             <Typography className={classes.imageWords}>Converse with anyone with any language</Typography>
           </Grid>
@@ -145,7 +46,7 @@ const Login = (props) => {
 
       <Grid className={classes.mainScreen}> 
           <Grid container item className={classes.header}>
-            <Typography className={classes.textGrey}>Don't have an account?</Typography>
+            <Typography className={classes.textGreyWithMargin}>Don't have an account?</Typography>
             <Button variant="contained" size="large" className={classes.secondaryButton}  onClick={() => history.push("/register")}>Create Account</Button>
           </Grid>
           <form onSubmit={handleLogin} class={classes.form}>
@@ -153,7 +54,7 @@ const Login = (props) => {
             <Grid class={classes.innerForm}>
               <Grid display="flex">
               <Typography className={classes.textLarge}>Welcome Back!</Typography>
-                <FormControl margin="normal" required fullWidth>
+                <FormControl margin="normal" required className={classes.formField}>
                   <TextField
                     aria-label="Username"
                     label="Username"
@@ -163,7 +64,7 @@ const Login = (props) => {
                   />
                 </FormControl>
               </Grid>
-              <FormControl margin="normal" required fullWidth>
+              <FormControl margin="normal" required className={classes.formField}>
                 <TextField
                   label="Password"
                   aria-label="Password"
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -12,107 +12,8 @@ import {
   Icon,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
-import backgroundImage from "./images/bg.png" 
 import bubble from "./images/bubble.svg" 
-import { makeStyles } from "@material-ui/core/styles";
-
-
-const useStyles = makeStyles((theme) => ({
-
-  root: {
-    display: "flex",
-    height: "100vh",
-    [theme.breakpoints.down("600")]: {
-      display: "grid",
-    }
-  },
-  image:{ 
-    display: "flex",
-    flexGrow: 1,
-    //converted the colors to rgba since it supports opacity better
-    backgroundImage: "linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)), url(" + backgroundImage + ")",
-    backgroundRepeat: "no-repeat;",
-    backgroundSize: "cover",
-    alignItems: "center", 
-    justifyContent: "center",
-    maxWidth: "40%",
-    [theme.breakpoints.down("600")]: {
-      maxWidth: "100%"
-    }
-  },
-
-  imageText:{ 
-    margin: 5,
-    alignItems: "center", 
-    justifyContent: "center",
-    textAlign: "center"
-  },
-  imageWords:{ 
-    fontSize: 40,
-    color: "white",
-    margin: 40
-  },
-
-  mainScreen:{ 
-    flexGrow: 3 ,
-    height: "80%",
-    padding: 40,
-  },
-
-  form:{
-    display: "flex",
-    flexDirection: "column",
-    height: "80%",
-    justifyContent: "center",
-  },
-
-  innerForm:{
-    margin: 10,
-  },
-
-  header:{
-    justifyContent:"flex-end",
-    alignItems: "center",
-    [theme.breakpoints.down("600")]: {
-      justifyContent:"center",
-      marginBottom: "20px"
-    }
-  },
-
-  secondaryButton:{
-    color: "#3A8DFF",
-    backgroundColor: "white",
-    padding: "12px 28px"
-  },
-
-  textLarge:{
-    fontSize: 45,
-    fontWeight: "bold",
-    [theme.breakpoints.down("600")]: {
-      fontSize: 30,
-    }
-  },
-
-  textGrey:{
-  color: "grey",
-  paddingRight: 20
-  },
-
-  primaryButtonContainer:{
-    display: "flex",
-    marginTop: 50,
-    justifyContent:"center",
-  },
-
-  primaryButton:{
-    color: "white",
-    backgroundColor: "#3A8DFF",
-    padding: "12px 70px",
-    fontSize: 20,
-    fontWeight: "bold",
-  }
-
-}));
+import { useStyles } from './styles'; 
 
 
 const Login = (props) => {
@@ -145,25 +46,23 @@ const Login = (props) => {
       <Grid className={classes.image}>
         <Grid className={classes.imageText}>
           <Icon>
-            <img src={bubble} alt="bubble" height="100" width="100"/>
+            <img src={bubble} alt="bubble" class={classes.icon}/>
           </Icon>
         <Typography className={classes.imageWords}>Converse with anyone with any language</Typography>
       </Grid>
     </Grid>
 
-      <Box className={classes.mainScreen} fullWidth>
+      <Box className={classes.mainScreen}>
 
         <Grid container item className={classes.header}>
           <Typography className={classes.textGrey}>Need to log in?</Typography>
           <Button variant="contained" size="large" className={classes.secondaryButton} onClick={() => history.push("/login")}>Login</Button>
         </Grid>
-
-
         <form onSubmit={handleRegister} class={classes.form}>
           <Grid class={classes.innerForm}>
-            <Grid display="flex"> 
-              <FormControl fullWidth>
-                <Typography className={classes.textLarge}>Create an Account.</Typography>
+          <Typography className={classes.textLarge}>Create an Account.</Typography>
+            <Grid> 
+              <FormControl className={classes.formField} >
                   <TextField
                     aria-label="username"
                     label="Username"
@@ -175,7 +74,7 @@ const Login = (props) => {
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl fullWidth>
+              <FormControl  className={classes.formField}>
                 <TextField
                   label="E-mail address"
                   aria-label="e-mail address"
@@ -187,7 +86,7 @@ const Login = (props) => {
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl fullWidth error={!!formErrorMessage.confirmPassword}>
+              <FormControl className={classes.formField} error={!!formErrorMessage.confirmPassword}>
                 <TextField
                   aria-label="password"
                   label="Password"
@@ -203,7 +102,7 @@ const Login = (props) => {
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl fullWidth error={!!formErrorMessage.confirmPassword}>
+              <FormControl className={classes.formField} error={!!formErrorMessage.confirmPassword}>
                 <TextField
                   label="Confirm Password"
                   aria-label="confirm password"
